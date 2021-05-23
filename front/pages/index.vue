08c19ef4b549aebfdafb6769d502e41922da3d84
Ubuntu <ubuntu@ip-172-31-29-103.ap-northeast-1.compute.internal>
2021-05-23 08:13:09 +0000

    first commit.

---
+++ front/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-btn
+      @click="getApi()"  
+    >
+     GET API
+    </v-btn>
+    <div>
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    getApi() {
+      this.$axios.get('http://localhost:8080/api/v1/hello')
+        .then((res) => {
+          this.message = res.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
