a28cd8bca929edef2aed4c34d8555bd4896266cc
Ubuntu <ubuntu@ip-172-31-29-103.ap-northeast-1.compute.internal>
2021-06-02 06:15:24 +0000

    make login page and fix header

--- front/components/common/Header.vue
+++ front/components/common/Header.vue
@@ -3,17 +3,11 @@
     <v-app-bar app>
         <v-btn href="/" depressed>Noteのパクリ</v-btn>
         <v-spacer />
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-list class="pa-0">
-        <v-btn nuxt href="login">ログイン</v-btn></v-btn>
-        <v-btn nuxt href="create">新規登録</v-btn></v-btn>
-      </v-list>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn nuxt href="login" depressed>ログイン</v-btn></v-btn>
+        <v-btn nuxt href="create" color="#2cb696" class="white--text" depressed>新規登録</v-btn></v-btn>
       <template v-slot:extension v-if="isHome">
         <v-tabs align-with-title>
           <v-tab>ホーム</v-tab>
@@ -34,10 +28,10 @@ export default {
     }
   },
   computed: {
+    /*global location*/
     isHome() {
-      /*global location*/
       return location.pathname == "/"
-    }
+    },
   }
 }
 </script>
--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Login Page</h1>
+    <v-container>
+        <v-card max-width="374" outlined class="pa-5 mx-auto">
+            <v-card-title>ログイン</v-card-title>
+            <v-text-field label="メールアドレス　または　note ID" color="#2cb696"></v-text-field>
+            <v-text-field label="パスワード" color="#2cb696"></v-text-field>
+            <v-btn depressed color="#2cb696" class="white--text" width="100%">ログイン</v-btn>
+        </v-card>
+    </v-container>
 </template>
+<script>
+    export default {
+        
+    }
+</script>
