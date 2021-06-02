b7b031413df77c1be87dde703adbce0c20eb22f9
Ubuntu <ubuntu@ip-172-31-29-103.ap-northeast-1.compute.internal>
2021-06-02 08:34:59 +0000

    サブヘッダのルーティング設定

--- front/components/common/Header.vue
+++ front/components/common/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-app-bar app>
+    <v-app-bar flat>
         <v-btn href="/" depressed>Noteのパクリ</v-btn>
         <v-spacer />
         <v-btn icon>
@@ -8,14 +8,28 @@
         </v-btn>
         <v-btn nuxt href="login" depressed>ログイン</v-btn></v-btn>
         <v-btn nuxt href="create" color="#2cb696" class="white--text" depressed>新規登録</v-btn></v-btn>
-      <template v-slot:extension v-if="isHome">
-        <v-tabs align-with-title>
-          <v-tab>ホーム</v-tab>
-          <v-tab>話題</v-tab>
-          <v-tab>募集中</v-tab>
+      <template v-slot:extension>
+        <v-tabs align-with-title v-model="tab">
+          <v-tab v-for="text in texts" :key="text.id" nuxt link :to="text.url">{{text.title}}</v-tab>
         </v-tabs>
+        <!--<Nuxt></Nuxt>-->
       </template>
     </v-app-bar>
+    
+    <!--<v-tabs-items v-model="tab">-->
+    <!--  <v-sheet>-->
+    <!--    <v-container height="300px">-->
+    <!--      <v-tab-item-->
+    <!--        v-for="item in items"-->
+    <!--        :key="item"-->
+    <!--      >-->
+    <!--        <v-card flat>-->
+    <!--          <v-card-text v-text="text"></v-card-text>-->
+    <!--        </v-card>-->
+    <!--      </v-tab-item>-->
+    <!--    </v-container>-->
+    <!--    </v-sheet>-->
+    <!--</v-tabs-items>-->
   </v-card>
 </template>
 
@@ -25,13 +39,20 @@
 export default {
   data() {
     return {
+      tab: null,
+      items: ["HOME", "話題", "サークル"],
+      texts: [
+        {title: "1", url: "/"},
+        {title: "2", url: "/create"},
+        {title: "3", url: "/login"},
+      ]
     }
   },
   computed: {
     /*global location*/
-    isHome() {
-      return location.pathname == "/"
-    },
+    // isHome() {
+    //   return location.pathname == "/"
+    // },
   }
 }
 </script>
---
+++ front/components/common/SubHeader.vue
@@ -0,0 +1,25 @@
+<!--      <template>-->
+<!--        <v-tabs align-with-title v-model="tab">-->
+<!--          <v-tab v-for="item in items" :key="item">{{item}}</v-tab>-->
+<!--          <v-tab-item v-for="text in texts" :key="text">{{text}}</v-tab-item>-->
+<!--        </v-tabs>-->
+<!--      </template>-->
+      
+<!--      <script>-->
+/*global $*/
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      tab: null,-->
+<!--      items: ["HOME", "話題", "サークル"],-->
+<!--      texts: ["1", "2", "3"]-->
+<!--    }-->
+<!--  },-->
+<!--  computed: {-->
+    /*global location*/
+    // isHome() {
+    //   return location.pathname == "/"
+    // },
+<!--  }-->
+<!--}-->
+<!--</script>-->
