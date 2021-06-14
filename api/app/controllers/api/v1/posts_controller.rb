7b733d56aae0a431405a36a5758f70b0513ab942
takeru-doi-0210 <takeru_doi@acceluniverse.com>
2021-06-14 14:35:30 +0900

    connect front and api

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class HelloController < ApplicationController
       def index
-        render json: 'Hello Rails'
       end
     end
   end
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Homeページ</h1>
+  <div>
+    <h1>Homeページ</h1>
+    <button @click="getApi()">GET API</button>
+    <p>{{message}}</p>
+  </div>
 </template>
+
+<script>
+export default{
+  data: () => {
+    return {
+      message: '',
+    }
+  },
+  methods: {
+    getApi() {
+      const url = "/api/v1/hello"
+      this.$axios.get(url)
+      .then((res) => {
+        this.message = "aaa"
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+
+}
+</script>
